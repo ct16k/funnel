c0718da0e62aeec380645e087fbe245eb66be0cd
Theodor Ciobanu <theodor.ciobanu@emag.ro>
2016-01-25 18:05:32 +0200

    add a 10s timeout to requests

--- plugin/shared.js
+++ plugin/shared.js
@@ -1,6 +1,9 @@
 var version = require('../version')
 module.exports = {
     fromJsonUrl: function (url, callback) {
+        var timeout_wrapper = function(req) {
+            return function() { req.abort(); }
+        }
         var parsed = require('url').parse(url);
         switch (parsed.protocol) {
             case 'http:':
@@ -14,21 +17,30 @@ module.exports = {
                 throw "Unsupported protocol in " + url;
         }
         parsed['user-agent'] = 'Funnel/' + version + ' node.js/' + process.version
-        protmod.get(parsed, function(res) {
+        var request = protmod.get(parsed, function(res) {
             var body = '';
             res.setEncoding('utf8');
             res.on('data', function (chunk) {
                 body += chunk;
+                clearTimeout(timeout);
+                timeout = setTimeout(fn, 10000);
             });
             res.on('end', function() {
                 try {
+                    clearTimeout(timeout);
                     body = JSON.parse(body);
                 } catch (e) {
                     throw "Invalid JSON returned from " + url;
                 }
                 callback(body, parsed);
             });
+            res.on('error', function(err) {
+                clearTimeout(timeout);
+                callback(err, null);
+            });
         });
+        var fn = timeout_wrapper(request);
+        var timeout = setTimeout(fn, 10000);
     },
     dbiSolo: function (result) {
         if (result && result[0]) {
